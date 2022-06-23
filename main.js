aec385c3d4201d830c8e3493dd14d4dd48f7b936
DanielMalede <danielmalede@gmail.com>
2022-06-23 10:54:03 +0300

    .

---
+++ main.js
@@ -0,0 +1,33 @@
+// function somefunc() {
+//     const mydate = new Date()
+//     return new Promise ((resolve,reject)=>{
+//         if (mydate.getHours()<11) {
+//             resolve("good morning good luck")
+//         }
+//         reject("good after noon")
+//     })
+// }
+// somefunc()
+//     .then((data)=>{h1_line.innerText=data})
+//     .catch((data)=>{h1_line.innerText=data})
+
+// function chackNumbers(num1, num2) {
+//     return new Promise((resolve, reject) => {
+//         if (num1 % num2 == 0) {
+//             resolve(h1_line.innerText = "yes its good")
+//         }
+//         else {
+//             reject(h1_line.innerText = "not divided")
+//         }
+//     })
+// }
+// chackNumbers(10,3)
+//     .then((data)=>{h1_line.innerText=data})
+//     .catch((data)=>{h1_line.innerText=data})
+
+
+// function name(params) {
+    
+// }
+
+
