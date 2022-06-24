b02aa40615e3f094c73c8ed6f296711f2415d843
DanielMalede <danielmalede@gmail.com>
2022-06-24 20:08:31 +0300

    .

--- main.js
+++ main.js
@@ -1,33 +1,69 @@
 // function somefunc() {
 //     const mydate = new Date()
-//     return new Promise ((resolve,reject)=>{
-//         if (mydate.getHours()<11) {
+//     return new Promise((resolve, reject) => {
+//         if (mydate.getHours() > 11) {
 //             resolve("good morning good luck")
 //         }
+//         else{
 //         reject("good after noon")
+//         }
 //     })
+//     .then((data) => { h1_line.innerText = data })
+//     .catch((err) => { h1_line.innerText = err })
 // }
-// somefunc()
-//     .then((data)=>{h1_line.innerText=data})
-//     .catch((data)=>{h1_line.innerText=data})
+
 
 // function chackNumbers(num1, num2) {
 //     return new Promise((resolve, reject) => {
 //         if (num1 % num2 == 0) {
-//             resolve(h1_line.innerText = "yes its good")
+//             resolve("yes its good")
 //         }
 //         else {
-//             reject(h1_line.innerText = "not divided")
+//             reject("not divided")
 //         }
 //     })
 // }
 // chackNumbers(10,3)
 //     .then((data)=>{h1_line.innerText=data})
-//     .catch((data)=>{h1_line.innerText=data})
+//     .catch((err)=>{h1_line.innerText=err})
 
 
-// function name(params) {
-    
+// function chackIfNumInArray(num1, myArray) {
+//     return new Promise((resolve, reject) => {
+//         if (myArray.includes(num1)) {
+//             resolve("yes")
+//         }
+//         reject("no")
+//     })
 // }
+// let myBTN3 = document.getElementById("myBTN3")
+// myBTN3.addEventListener("click", chackIfNumInArray(2, [4, 7, 5, 2])
+//     .then((data) => { h1_line.innerText = data })
+//     .catch((err) => { h1_line.innerText = err }))
 
+// async function somefunc() {
+//     try {
+//         return await chackIfNumInArray(2,[4,5,2])
+//             .then((data) => { h1_line.innerText = data })
+//             .catch((err) => { h1_line.innerText = err })
+//     }
+//     catch(err){
 
+//     }
+//     finally{
+
+//     }
+// }
+// somefunc()
+
+async function getAdrresses() {
+    try{
+        await fetch("https://moviesmern.herokuapp.com/movies/all")
+        .then(response=>{
+            return response.json();
+        })
+        // .then((res)=>res.data.forEach(element => {
+            
+        // });)
+    }
+}
